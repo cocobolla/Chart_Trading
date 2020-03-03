ae276fe45eb798ef66c7b943127faf9b8d66e744
Cocobolla <pcal1029@gmail.com>
2020-03-03 23:55:19 +0900

    Backtesting Code(Only PSAR)

--- draw_training_image.py
+++ draw_training_image.py
@@ -1,7 +1,3 @@
-import keras
-from keras import layers, models
-from keras import backend
-from keras import datasets
 import matplotlib.pyplot as plt
 import cv2
 import os
@@ -9,6 +5,7 @@ import numpy as np
 import pandas as pd
 from mpl_finance import candlestick2_ohlc
 import matplotlib.ticker as ticker
+import datetime
 
 
 def draw_chart(csv_path, output_dir, labeling_ma=5, window=60):
@@ -18,16 +15,46 @@ def draw_chart(csv_path, output_dir, labeling_ma=5, window=60):
     print("Max: {:.2f}, Min:{:.2f}".format(df['Price'].max(), df['Price'].min()))
     print("Date: {}".format(len(df)))
     print("Scaler: {:.2f}".format(scaler))
+    total_ret = psar_backtesting(df)
 
     for i in range(labeling_ma, len(df) - window - labeling_ma):
+        # Draw Candle Stick
         fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot(1, 1, 1)
         ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
         target_df = df.loc[df.index[i]: df.index[i + window], :]
-        ax.axis('off')
+        # ax.axis('off')
         candlestick2_ohlc(ax, target_df['Open'], target_df['High'], target_df['Low'], target_df['Price'],
                           width=0.5, colorup='r', colordown='b')
 
+        # Draw PSAR
+        psar_dict = get_psar(target_df)
+        psar = psar_dict['Psar']
+        # psar = target_df.Price
+        ax.plot(np.arange(len(target_df.index)), psar, linestyle='', marker='*')
+
+        # Draw Return
+        target_df = df.loc[df.index[i]: df.index[i + window], :]
+        ret = total_ret.loc[total_ret.index[i]: total_ret.index[i + window]]
+        ax2 = ax.twinx()
+        ax2.set_ylim(total_ret.min()*99/100, total_ret.max())
+        ax2.bar(np.arange(len(target_df.index)), ret, alpha=0.3)
+
+        # Set x-axis as Date
+        _xticks = list()
+        _xlabels = list()
+        _wd_prev = 0
+        _close = list()
+        for _x, d in zip(np.arange(len(target_df.index)), target_df.index.values):
+            weekday = datetime.datetime.strptime(str(d).split('T')[0], '%Y-%m-%d').weekday()
+            if weekday <= _wd_prev:
+                _xticks.append(_x)
+                _xlabels.append(datetime.datetime.strptime(str(d).split('T')[0], '%Y-%m-%d').strftime('%y/%m/%d'))
+            _wd_prev = weekday
+        ax.set_xticks(_xticks)
+        ax.set_xticklabels(_xlabels, rotation=45, minor=False)
+
+        # Tagging the name of Candle Chart with MA Gradient
         # close_now = df.loc[df.index[i + window], 'Price']
         # close_future = df.loc[df.index[i + window + labeling_ma], 'Price']
         # ma_gradient = round((close_future - close_now) / labeling_ma, 2)
@@ -41,22 +68,61 @@ def draw_chart(csv_path, output_dir, labeling_ma=5, window=60):
         output_path = os.path.join(output_dir, output_name)
 
         print("Printing {}th image".format(i - labeling_ma + 1))
+        # plt.show()
         fig.savefig(output_path)
         plt.close(fig)
 
 
-def psar(barsdata, iaf=0.02, maxaf=0.2):
+def is_psar_bull(h, l, psar):
+    if psar >= (h + l)/2:
+        return False
+    elif psar <= (h + l)/2:
+        return True
+    else:
+        print(h, l, psar)
+        raise
+
+
+def psar_backtesting(barsdata):
+    initial_af = 0.02
+    max_af = 0.2
+    psar_dict = get_psar(barsdata)
+    barsdata['Psar'] = psar_dict['Psar']
+    # barsdata['Psar'] = get_psar(barsdata, initial_af=initial_af, max_af=max_af)['Psar']
+    barsdata['Is_bull'] = barsdata.apply(lambda x: is_psar_bull(x['High'], x['Low'], x['Psar']), axis=1)
+    barsdata['Return'] = 1.0
+
+    for i in range(60, len(barsdata)):
+        prev_row_index = barsdata.index[i-1]
+        row_index = barsdata.index[i]
+        prev_status = barsdata.loc[prev_row_index, 'Is_bull']
+        status = barsdata.loc[row_index, 'Is_bull']
+        bull = True
+        bear = False
+
+        if prev_status == bull:
+            # print(barsdata.loc[prev_row_index, 'Return'] * barsdata.loc[row_index, 'Price'] / barsdata.loc[row_index, 'Price'])
+            barsdata.loc[row_index, 'Return'] = \
+                barsdata.loc[prev_row_index, 'Return'] * barsdata.loc[row_index, 'Price'] / barsdata.loc[prev_row_index, 'Price']
+        if prev_status == bear:
+            # print(barsdata.loc[prev_row_index, 'Return'] * barsdata.loc[row_index, 'Price'] / barsdata.loc[row_index, 'Price'])
+            barsdata.loc[row_index, 'Return'] = barsdata.loc[prev_row_index, 'Return']
+
+    return barsdata['Return']
+
+
+def get_psar(barsdata, initial_af=0.02, max_af=0.2):
     length = len(barsdata)
-    dates = list(barsdata['Date'])
+    dates = list(barsdata.index)
     high = list(barsdata['High'])
     low = list(barsdata['Low'])
-    close = list(barsdata['Close'])
+    close = list(barsdata['Price'])
     psar = close[0:len(close)]
     psarbull = [None] * length
     psarbear = [None] * length
     bull = True
-    af = iaf
-    ep = low[0]
+    af = initial_af
+    # ep = low[0]
     hp = high[0]
     lp = low[0]
 
@@ -74,20 +140,20 @@ def psar(barsdata, iaf=0.02, maxaf=0.2):
                 reverse = True
                 psar[i] = hp
                 lp = low[i]
-                af = iaf
+                af = initial_af
         else:
             if high[i] > psar[i]:
                 bull = True
                 reverse = True
                 psar[i] = lp
                 hp = high[i]
-                af = iaf
+                af = initial_af
 
         if not reverse:
             if bull:
                 if high[i] > hp:
                     hp = high[i]
-                    af = min(af + iaf, maxaf)
+                    af = min(af + initial_af, max_af)
                 if low[i - 1] < psar[i]:
                     psar[i] = low[i - 1]
                 if low[i - 2] < psar[i]:
@@ -95,7 +161,7 @@ def psar(barsdata, iaf=0.02, maxaf=0.2):
             else:
                 if low[i] < lp:
                     lp = low[i]
-                    af = min(af + iaf, maxaf)
+                    af = min(af + initial_af, max_af)
                 if high[i - 1] > psar[i]:
                     psar[i] = high[i - 1]
                 if high[i - 2] > psar[i]:
@@ -106,14 +172,14 @@ def psar(barsdata, iaf=0.02, maxaf=0.2):
         else:
             psarbear[i] = psar[i]
 
-    return {"dates": dates, "high": high, "low": low, "close": close, "psar": psar, "psarbear": psarbear,
-            "psarbull": psarbull}
+    return {"Dates": dates, "High": high, "Low": low, "Close": close, "Psar": psar, "Psarbear": psarbear,
+            "Psarbull": psarbull}
 
 
 if __name__ == '__main__':
     root_path = r'C:\Users\USER\workspace\KSIF\Chart_Trading'
     data_dir = 'data'
-    output_dir = 'Future_ma20_Labeling_scaling'
+    output_dir = 'Future_ma20_Labeling_scaling_psar'
     csv_path = os.path.join(root_path, data_dir, 'TSLA Historical Data.csv')
     output_dir_path = os.path.join(root_path, output_dir)
 
